633310e1d2a43b48910b31c6b903e51fb0b73c70
Alex Skal <sanderoffsky@gmail.com>
2021-06-04 17:21:12 +0300

    form

---
+++ src/ProductForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class ProductForm extends React.Component {
+    state = {
+        address: ""
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ProductForm
